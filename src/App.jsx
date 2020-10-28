a4c6aef35529e3863422526677548c54fa072f34
e-floran <e-floran@hotmail.fr>
2020-10-28 11:11:45 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,14 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 import EventsFinder from './components/EventsFinder';
 
-const options = {
-  method: 'GET',
-  url: 'https://eonet.sci.gsfc.nasa.gov/api/v3/events',
-  params: { status: 'open' },
-  headers: { 'Content-Type': 'application/json' },
-};
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      eventsList: [],
-    };
-    this.getEvents = this.getEvents.bind(this);
-  }
-
-  componentDidMount() {
-    this.getEvents();
-  }
-
-  getEvents() {
-    axios
-      .request(options)
-      .then((response) => response.data.events)
-      .then((events) => {
-        this.setState({
-          eventsList: events,
-        });
-      });
-  }
-
-  render() {
-    const { eventsList } = this.state;
-    return (
-      <div className="App">
-        <button type="button" onClick={this.getEvents}>
-          Get events
-        </button>
-        {eventsList.map((event) => (
-          <EventsFinder eventsImport={event} />
-        ))}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <EventsFinder />
+    </div>
+  );
 }
 
 export default App;
--- src/components/EventsFinder.jsx
+++ src/components/EventsFinder.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import axios from 'axios';
+// import PropTypes from 'prop-types';
 
-function EventsFinder({ eventsImport }) {
-  return (
-    <div>
-      <p>{eventsImport.title}</p>
-      <p>{eventsImport.geometry[0].coordinates}</p>
-    </div>
-  );
+const options = {
+  method: 'GET',
+  url: 'https://eonet.sci.gsfc.nasa.gov/api/v3/events',
+  params: { status: 'open' },
+  headers: { 'Content-Type': 'application/json' },
+};
+
+class EventsFinder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventsList: [],
+    };
+    this.getEvents = this.getEvents.bind(this);
+  }
+
+  componentDidMount() {
+    this.getEvents();
+  }
+
+  getEvents() {
+    axios
+      .request(options)
+      .then((response) => response.data.events)
+      .then((events) => {
+        this.setState({
+          eventsList: events,
+        });
+      });
+  }
+
+  render() {
+    const { eventsList } = this.state;
+    return (
+      <div>
+        <button type="button" onClick={this.getEvents}>
+          Get events
+        </button>
+        <p>{eventsList.title}</p>
+        <p>{eventsList.geometry[0].coordinates}</p>
+      </div>
+    );
+  }
 }
 
-EventsFinder.propTypes = {
-  eventsImport: PropTypes.shape.isRequired,
-  // title: PropTypes.string.isRequired,
-  // geometry: PropTypes.string.isRequired,
-  // coordinates: PropTypes.string.isRequired,
-};
+// EventsFinder.propTypes = {
+//   eventsList: PropTypes.shape.isRequired,
+//   title: PropTypes.string.isRequired,
+//   geometry: PropTypes.string.isRequired,
+//   coordinates: PropTypes.string.isRequired,
+// };
 
 export default EventsFinder;
