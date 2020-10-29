e026936133199038e4a3638231a321b685a6bb33
e-floran <e-floran@hotmail.fr>
2020-10-30 00:17:50 +0100

    united all functions into one

--- src/lib/EventsFinder.js
+++ src/lib/EventsFinder.js
@@ -1,20 +1,41 @@
 import axios from 'axios';
-// Donner un input sous la forme d'un objet {params:{clé: "valeur"}}.
+// Donner un input sous la forme d'un objet {field: "events", params:{clé: "valeur"}, categorie:"categorie"}.
 // Pour events, les paramètres sont : source, status, limit, days, start, end, magID, magMin, magMax, bbox.
+// Pour geoJson, les paramètres sont : source, status, limit, days, start, end, magID, magMin, magMax, bbox.
+// Pour categories, les paramètres sont : source, status, limit, days.
 
 function eventsFinder(input) {
   const options = {
     method: 'GET',
-    url: 'https://eonet.sci.gsfc.nasa.gov/api/v3/events',
-    params: {},
     headers: { 'Content-Type': 'application/json' },
   };
 
-  if (input && input !== {}) {
-    options.params = input;
+  if (input.field === 'events') {
+    options.url = 'https://eonet.sci.gsfc.nasa.gov/api/v3/events';
+    if (input.params && input.params !== {}) {
+      options.params = input.params;
+    }
+    return axios.request(options).then((response) => response.data.events);
   }
-
-  return axios.request(options).then((response) => response.data.events);
+  if (input.field === 'geoJson') {
+    options.url = 'https://eonet.sci.gsfc.nasa.gov/api/v3/events/geojson';
+    if (input.params && input.params !== {}) {
+      options.params = input.params;
+    }
+    return axios.request(options).then((response) => response.data.features);
+  }
+  if (input.field === 'categories') {
+    options.url = `https://eonet.sci.gsfc.nasa.gov/api/v3/categories/${input.categorie}`;
+    if (input.params && input.params !== {}) {
+      options.params = input.params;
+    }
+    return axios.request(options).then((response) => response.data.events);
+  }
+  if (input.field === 'layers') {
+    options.url = `https://eonet.sci.gsfc.nasa.gov/api/v3/layers/${input.categorie}`;
+    return axios.request(options).then((response) => response.data.categories);
+  }
+  return 'Something went wrong.';
 }
 
 export default eventsFinder;
