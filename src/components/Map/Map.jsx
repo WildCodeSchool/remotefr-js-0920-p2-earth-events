43ec7e71662179a823e55a97e7cf3902c8c3c6f5
e-floran <e-floran@hotmail.fr>
2020-11-04 13:46:13 +0100

    added select menu to change map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,38 +2,117 @@ import React from 'react';
 import L from 'leaflet';
 import './CSS/map.css';
 
+const lightMap = {
+  tileLayer: 'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png',
+  maxZoom: 20,
+  attribution:
+    '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+};
+const darkMap = {
+  tileLayer:
+    'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+  maxZoom: 20,
+  attribution:
+    '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+};
+const satMap = {
+  tileLayer:
+    'https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}',
+  attribution:
+    '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
+  bounds: [
+    [-75, -180],
+    [81, 180],
+  ],
+  minZoom: 2,
+  maxZoom: 19,
+  apikey: 'choisirgeoportail',
+  format: 'image/jpeg',
+  style: 'normal',
+};
+
 export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 'light map' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   componentDidMount() {
     this.map = L.map('wildmap').setView([45.184094, 0.716364], 6);
+    L.tileLayer(this.mapPicker('tileLayer'), {
+      attribution: this.mapPicker('attribution'),
+      minZoom: 1,
+      maxZoom: this.mapPicker('maxZoom'),
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        'pk.eyJ1IjoiZmxvd3JhbiIsImEiOiJja2gweGV1bGkwMnFrMnhxeTRvbGVsdmJ2In0.PcExAwc7ssv0VbrNB_sbtg',
+      bounds: [
+        [-75, -180],
+        [81, 180],
+      ],
+      apikey: 'choisirgeoportail',
+      format: 'image/jpeg',
+      style: 'normal',
+    }).addTo(this.map);
+  }
 
-    L.tileLayer(
-      'https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}',
-      {
-        attribution:
-          'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
-        // From bounds to titlematrixset, these params are specific to this map (except for the maxZoom).
-        bounds: [
-          [-85.0511287776, -179.999999975],
-          [85.0511287776, 179.999999975],
-        ],
-        minZoom: 1,
-        maxZoom: 8,
-        format: 'jpg',
-        time: '',
-        tilematrixset: 'GoogleMapsCompatible_Level',
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken:
-          'pk.eyJ1IjoiZmxvd3JhbiIsImEiOiJja2gweGV1bGkwMnFrMnhxeTRvbGVsdmJ2In0.PcExAwc7ssv0VbrNB_sbtg',
-      },
-    ).addTo(this.map);
+  componentDidUpdate() {
+    L.tileLayer(this.mapPicker('tileLayer'), {
+      attribution: this.mapPicker('attribution'),
+      minZoom: 1,
+      maxZoom: this.mapPicker('maxZoom'),
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        'pk.eyJ1IjoiZmxvd3JhbiIsImEiOiJja2gweGV1bGkwMnFrMnhxeTRvbGVsdmJ2In0.PcExAwc7ssv0VbrNB_sbtg',
+      bounds: [
+        [-75, -180],
+        [81, 180],
+      ],
+      apikey: 'choisirgeoportail',
+      format: 'image/jpeg',
+      style: 'normal',
+    }).addTo(this.map);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  mapPicker(param) {
+    const { value } = this.state;
+    switch (value) {
+      case 'light map':
+        return lightMap[param];
+      case 'dark map':
+        return darkMap[param];
+      case 'Satellite map':
+        return satMap[param];
+      default:
+        return lightMap[param];
+    }
   }
 
   render() {
+    const { value } = this.state;
     return (
       <div className="mapContainer">
-        <div id="wildmap" />
+        <div id="wildmap">
+          <select
+            name="mapSelector"
+            className="mapSelector"
+            value={value}
+            onChange={this.handleChange}
+          >
+            <option value="light map">Light map</option>
+            <option value="dark map">Dark map</option>
+            <option value="Satellite map">Satellite view</option>
+          </select>
+        </div>
       </div>
     );
   }
