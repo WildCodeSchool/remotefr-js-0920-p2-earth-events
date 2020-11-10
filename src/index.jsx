85427926f17f8e27bcf8589969a93e71c9cb9e2c
Clovis Gauzy <github.788acf@clovisgauzy.fr>
2020-11-10 09:51:41 +0100

    Connect the Reducers to the Store

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './redux/reducers';
 import './index.css';
 import App from './App';
 
-const store = createStore(() => {});
+const store = createStore(
+  reducers,
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // Connect to browser plugin => https://github.com/zalmoxisus/redux-devtools-extension
+);
 
 ReactDOM.render(
   <React.StrictMode>
