bc0649600f4dda1b01b1b3f6c490d9bc98f10e8b
Clovis Gauzy <github.788acf@clovisgauzy.fr>
2020-11-14 17:57:42 +0100

    func App to class App
    lastVisit must be set earlier than componentDidMount (via useEffect) or the Map will crash (bad bounds ? overloaded map ? idk, using class component just did fix it for now).

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // import './App.css';
 import SideNav from './components/SideNav/SideNav';
 import Map from './components/Map/Map';
 
-function App() {
-  const [lastVisit, setLastVisit] = useState('');
-  useEffect(() => {
-    if (window.localStorage.getItem('lastVisit')) {
-      setLastVisit(window.localStorage.getItem('lastVisit').slice(0, 10));
-    } else {
-      setLastVisit('');
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let lastVisit = '';
+    if (window.localStorage.getItem('lastVisit'))
+      lastVisit = window.localStorage.getItem('lastVisit').slice(0, 10);
+    this.state = { lastVisit };
+
     window.localStorage.setItem('lastVisit', new Date().toISOString());
-  }, []);
-  return (
-    <div>
-      <SideNav lastVisit={lastVisit} />
-      <Map />
-    </div>
-  );
+  }
+
+  render() {
+    const { lastVisit } = this.state;
+    return (
+      <div>
+        <SideNav lastVisit={lastVisit} />
+        <Map />
+      </div>
+    );
+  }
 }
 
 export default App;
