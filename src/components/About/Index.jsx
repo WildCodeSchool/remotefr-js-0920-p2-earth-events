26c915b4a17149ad0d6a7971baeb8f6f33d65bb7
e-floran <e-floran@hotmail.fr>
2020-11-26 10:21:55 +0100

    created about

---
+++ src/components/About/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        This application has been created by Alexandre Chauvin, Floran Eiclies
+        and Clovis Gauzy.
+      </p>
+      <p>We used React, Leaflet, the Eonet API and a little bit of skills.</p>
+    </div>
+  );
+}
+export default About;
